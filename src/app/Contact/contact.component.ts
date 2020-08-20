e5bc01a8dc18db57acd91dd423514ee7a1a9b49a
cwaldron97 <colin-waldron@uiowa.edu>
2020-08-20 10:49:35 -0500

    still trying to get routerlink to work properly

--- src/app/Contact/contact.component.ts
+++ src/app/Contact/contact.component.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
   selector: 'app-contact',
   templateUrl: './contact.component.html'
 })
+
 export class ContactComponent {
   constructor() {
   }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent} from './Products/products.component';
 import { ContactComponent } from './Contact/contact.component';
 import { AboutComponent} from './About/about.component';
+import {AppComponent} from './app.component';
 
 const routes: Routes = [
+  {path: 'home', component: AppComponent},
   {path: 'products', component: ProductsComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'about', component: AboutComponent}
